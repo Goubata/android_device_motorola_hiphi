248356df0c8063585e6a127526db1e00afa44b4b
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-07-14 18:14:54 +0900

    Update twrp_hiphi.mk

--- twrp_hiphi.mk
+++ twrp_hiphi.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/motorola/hiphi/device.mk)
 
 PRODUCT_DEVICE := hiphi
-PRODUCT_NAME := omni_hiphi
+PRODUCT_NAME := twrp_hiphi
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 30 pro
 PRODUCT_MANUFACTURER := motorola
