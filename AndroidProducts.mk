f886f6587ef1ea8e3a29816a6070e64fb61b4acc
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-07-18 00:18:06 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_hiphi.mk
+    $(LOCAL_DIR)/omni_hiphi.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_hiphi-user \
-    twrp_hiphi-userdebug \
-    twrp_hiphi-eng
+    omni_hiphi-user \
+    omni_hiphi-userdebug \
+    omni_hiphi-eng
