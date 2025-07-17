ae04d8567b632a60041ec404e6cf2cf22fdf61a8
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-07-18 00:15:58 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_hiphi-user
-add_lunch_combo twrp_hiphi-userdebug
-add_lunch_combo twrp_hiphi-eng
+add_lunch_combo omni_hiphi-user
+add_lunch_combo omni_hiphi-userdebug
+add_lunch_combo omni_hiphi-eng
